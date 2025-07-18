7f8ee3f79aef811738694547dac0b5b4cfe00368
Reda TRANKIL <reda.trankil@gmail.com>
2025-07-18 15:01:08 +0100

    styled leaflet map

--- app/R/modules/leaflet_map.R
+++ app/R/modules/leaflet_map.R
@@ -21,7 +21,18 @@ leaflet_map_server <- function(id, map_data) {
   moduleServer(id, function(input, output, session) {
     
     output$map <- renderLeaflet({
-      leaflet(map_data) %>%
+      leaflet(map_data, 
+              options = leafletOptions(
+                worldCopyJump = TRUE,  # Prevents multiple worlds
+                maxBounds = list(
+                  list(-90, -180),  # Southwest corner (lat, lng)
+                  list(90, 180)     # Northeast corner (lat, lng)
+                ),
+                maxBoundsViscosity = 1.0,  # How much to resist dragging outside bounds
+                minZoom = 2,  # Minimum zoom level to prevent seeing multiple worlds
+                maxZoom = 18,  # Optional: set maximum zoom level
+                attributionControl = FALSE
+              )) %>%
         addProviderTiles(providers$Esri.WorldImagery)%>%
         addProviderTiles(providers$CartoDB.VoyagerOnlyLabels)%>%
         setView(lng = -6.87, lat = 29 , zoom = 5)  # Adjust initial view as needed
